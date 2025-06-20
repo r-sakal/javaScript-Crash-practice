601ac296d20c9e780b330b168085fb077a4f7db9
Rob <rob.sakal23@gmail.com>
2025-06-20 10:48:12 -0400

    promise practice

--- app.js
+++ app.js
@@ -601,10 +601,25 @@ function getSubscriptionStatus() {
  *        -if otherwise reject("no video")
  * 4. console.log the result of getVideo(status) in main()
  */
+function getVideo(subscriptionStatus) {
+     return new Promise((resolve, reject) => {
+          if (subscriptionStatus === "VIP") {
+               resolve("show video")
+          }
+          else if (
+               subscriptionStatus === "Free") {
+                    resolve("show trailer")
+               }
+          else {
+               reject("no video")
+          }
+     })
+}
 
 async function main() {
           const status = (await getSubscriptionStatus())
           statusRef.innerHTML = status;
+          console.log(await getVideo(status))
 }
 
 main();
